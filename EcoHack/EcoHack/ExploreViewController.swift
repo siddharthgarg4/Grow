e08c867d67d28d5307c6d5830d58314bb0e37f56
siddharthtdlab <siddharth@tdlab.io>
2019-03-09 13:10:55 -0500

    created the project

--- EcoHack/EcoHack/ExploreViewController.swift
+++ EcoHack/EcoHack/ExploreViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ExploreViewController.swift
 //  EcoHack
 //
 //  Created by Siddharth Garg on 2019-03-09.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ExploreViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
-
-
 }
-
---
+++ EcoHack/EcoHack/ScannerClass.swift
@@ -0,0 +1,21 @@
+//
+//  ScannerClass.swift
+//  EcoHack
+//
+//  Created by Siddharth Garg on 2019-03-09.
+//  Copyright © 2019 GreenDreamTeam. All rights reserved.
+//
+
+import UIKit
+
+class ScannerClass: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
