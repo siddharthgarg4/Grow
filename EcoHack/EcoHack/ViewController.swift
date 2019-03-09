9f14e7fa5611fed4fa4e783e11e659def83360e1
siddharthtdlab <siddharth@tdlab.io>
2019-03-09 12:01:01 -0500

    pushing the project

---
+++ EcoHack/EcoHack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EcoHack
+//
+//  Created by Siddharth Garg on 2019-03-09.
+//  Copyright © 2019 GreenDreamTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
