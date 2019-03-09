311780cbfeca9b662dd988dddea439bc3b78243a
siddharthtdlab <siddharth@tdlab.io>
2019-03-09 17:50:36 -0500

    making the pop up work

---
+++ EcoHack/EcoHack/CompanyInformationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CompanyInformationViewController.swift
+//  EcoHack
+//
+//  Created by Siddharth Garg on 2019-03-09.
+//  Copyright © 2019 GreenDreamTeam. All rights reserved.
+//
+
+import UIKit
+
+class CompanyInformationViewController: UIViewController {
+
+    @IBOutlet weak var popUpView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
