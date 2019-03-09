47d2a89271ddc10cfbecf1d5865c626a39ec19d5
Andrew Drury <drurya99@gmail.com>
2019-03-09 15:41:36 -0500

    Setting up skelly

--- EcoHack/EcoHack/AppDelegate.swift
+++ EcoHack/EcoHack/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        window?.rootViewController = ArScannerViewController()
+        window?.rootViewController = Onboarding()
         return true
     }
 
