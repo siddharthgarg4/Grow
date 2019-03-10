3d2e542abded67c56d635faa9ca4bfa4bd36f589
siddharthtdlab <siddharth@tdlab.io>
2019-03-10 01:29:32 -0500

    creating the explore page

---
+++ EcoHack/EcoHack/CompanyCollectionCellView.swift
@@ -0,0 +1,18 @@
+//
+//  CompanyCollectionCellView.swift
+//  EcoHack
+//
+//  Created by Siddharth Garg on 2019-03-10.
+//  Copyright © 2019 GreenDreamTeam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class CompanyCollectionCellView: UICollectionViewCell {
+    @IBOutlet weak var companyImage: UIImageView!
+    @IBOutlet weak var informationView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
--- EcoHack/EcoHack/ExploreViewController.swift
+++ EcoHack/EcoHack/ExploreViewController.swift
@@ -9,23 +9,61 @@
 import UIKit
 import ARKit
 
-class ExploreViewController: UIViewController {
 
-    var scrollView: UIScrollView!
-    var imageView: UIImageView!
+
+
+class ExploreViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var companyCollection: UICollectionView!
+    
+    let cellIdentifier = "CompanyCollectionCellView"
+    let companyNum = 2
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        companyCollection.delegate = self
+        companyCollection.dataSource = self
+        let nibCell = UINib(nibName: cellIdentifier, bundle: nil)
+        companyCollection.register(nibCell, forCellWithReuseIdentifier: cellIdentifier)
+    }
         
-        imageView = UIImageView(image: UIImage(named: "consciousbychloePelaCaseCompostableBiodegradablePlasticFreeZeroWastePhoneCase8.jpg"))
-        
-        scrollView = UIScrollView(frame: view.bounds)
-        scrollView.backgroundColor = UIColor.white
-        scrollView.contentSize = imageView.bounds.size
-        scrollView.autoresizingMask = UIView.AutoresizingMask(rawValue: UIView.AutoresizingMask.flexibleWidth.rawValue | UIView.AutoresizingMask.flexibleHeight.rawValue)
-        
-        scrollView.addSubview(imageView)
-        view.addSubview(scrollView)
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return companyNum
+    }
+    
+    /*func collectionView(_ collectionView: UICollectionView,
+    layout collectionViewLayout: UICollectionViewLayout,
+    insetForSectionAt section: Int) -> UIEdgeInsets {
+    let inset = 6
+    return UIEdgeInsets(top: CGFloat(inset), left: CGFloat(inset), bottom: CGFloat(inset), right: CGFloat(inset))
+    }*/
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize.init(width: 350, height: 450)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! CompanyCollectionCellView
+        return cell
+        }
+    
+    func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath) {
+    /*let cell = collectionView.cellForItem(at: indexPath)
+    cell?.backgroundColor = UIColor(red: 53/255, green: 178/255, blue: 52/255, alpha: 1)
+    }
+    
+    // change background color when user touches cell
+    func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
+    let cell = collectionView.cellForItem(at: indexPath)
+    cell?.backgroundColor = UIColor.gray
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    // handle tap events
+    print("You selected cell #\(indexPath.item)!")*/
+
+    }
 }
