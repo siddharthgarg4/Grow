6acc1925d440301456595420206ebc24bfe10c92
Andrew Drury <drurya99@gmail.com>
2019-03-10 12:47:40 -0400

    created onboarding

---
+++ Grow/RealProfile.swift
@@ -0,0 +1,30 @@
+//
+//  RealProfile.swift
+//  Grow
+//
+//  Created by Andrew Drury on 2019-03-10.
+//  Copyright © 2019 GreenDreamTeam. All rights reserved.
+//
+
+import UIKit
+
+class RealProfile: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
